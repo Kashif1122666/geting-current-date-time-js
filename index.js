69cf866ac4ef15f09076ec34c10d15c156b535ac
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2024-08-02 15:43:16 -0700

    Add files via upload
    done

---
+++ index.js
@@ -0,0 +1,55 @@
+// Chapter 31: geting current date and time
+
+// Geting current date and Time, DAte Object
+//  const rightnow = new Date();
+//  console.log(rightnow);
+
+// Explain the date Object
+//  const rightnow = new Date();
+//  console.log(rightnow);
+
+// get different chunks from date Object
+
+// geting current time in miliseconds
+//  const rightnow = new Date();
+// const currentTime = rightnow.getTime(); 
+// console.log(currentTime);
+
+// geting current year 
+//  const rightnow = new Date();
+// const currentTime = rightnow.getFullYear(); 
+// console.log(currentTime);
+
+// geting current date 
+// const rightnow = new Date();
+// const currentDate = rightnow.getDate();
+// console.log(currentDate);
+
+// geting current month 
+// const rightnow = new Date();
+// const currentMonth = rightnow.getMonth();
+// console.log(currentMonth);
+
+// geting current day 
+// const rightnow = new Date();
+// const currentDay = rightnow.getDay();
+// console.log(currentDay);
+
+// convert date Object ro String using toString
+// const rightnow = new Date();
+// console.log(typeof rightnow);
+// const dateINString = rightnow.toString();
+// console.log(typeof dateINString);
+
+// get current day number
+// const rightnow = new Date();
+// console.log(rightnow);
+// const currentDay = rightnow.getDay();
+// console.log(currentDay);
+
+// get today name
+// const dayName = ["SUNDAY", "MONDAY", "TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY"];
+// const rightnow = new Date();
+// const dayNumber = rightnow.getDay();
+// const currentDay = dayName[dayNumber];
+// console.log(currentDay);
